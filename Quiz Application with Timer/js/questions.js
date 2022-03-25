bd5b6dfe0e55dca5a606ad4b08149fa6f589fb1f
Shreshtha Nag <71623963+Shreshtha1999@users.noreply.github.com>
2022-03-26 01:21:12 +0530

    Add files via upload

---
+++ Quiz Application with Timer/js/questions.js
@@ -0,0 +1,125 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  
+    {
+    numb: 6,
+    question: "Javascript is an _________ language?",
+    answer: "Object-Oriented",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Procedural",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following keywords is used to define a variable in Javascript?",
+    answer: "Both A and B",
+    options: [
+      "var",
+      "let",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which of the following methods is used to access HTML elements using Javascript?",
+    answer: "Both A and B",
+    options: [
+      "getElementbyId()",
+      "getElementbyClassName()",
+      "Both A and B",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 8,
+    question: "How can a datatype be declared to be a constant type?",
+    answer: "Const",
+    options: [
+      "Const",
+      "var",
+      "let",
+      "constant"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which of the following is not a Javascript framework?",
+    answer: "Cassandra",
+    options: [
+      "Node",
+      "Vue",
+      "React",
+      "Cassandra"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which of the following are closures in Javascript?",
+    answer: "Functions",
+    options: [
+      "Variables",
+      "Functions",
+      "Objects ",
+      "All of the above"
+    ]
+  }
+];
